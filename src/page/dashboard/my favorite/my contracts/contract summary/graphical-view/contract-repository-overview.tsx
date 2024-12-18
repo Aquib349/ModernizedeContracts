2ed8a3b6130def4a838494dade606c5fc1f9aadb
DeveloperPranaya <pranaya_p@optimusbt.net>
2024-12-18 18:05:29 +0530

    changes in code

--- src/page/dashboard/my favorite/my contracts/contract summary/graphical-view/contract-repository-overview.tsx
+++ src/page/dashboard/my favorite/my contracts/contract summary/graphical-view/contract-repository-overview.tsx
@@ -3,7 +3,7 @@ import PieChart from "@/components/chart/PieChart";
 
 function ContractRepositoryOverview() {
     return (
-        <div className="flex justify-around items-center">
+        <div className="flex justify-around items-center mt-4">
             <BarChart
                 title="Non-Standard/High-Risk Aspects"
                 seriesData={[3, 2]}
@@ -15,7 +15,7 @@ function ContractRepositoryOverview() {
                 title="Contract Distribution by Type"
                 series={[3, 4, 2, 1]}
                 labels={["Service Agreement", "Master Service Agreement", "Software Licensing", "Dealer Contract"]}
-                colors={["#F857C1", "#F53255", "#F46920", "#FFAF00"]} // Custom colors
+                colors={["#F857C1", "#F53255", "#F46920", "#FFAF00"]} 
             />
         </div>
     );
