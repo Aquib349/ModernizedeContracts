b9292130fe0f741d205e89deebecfe1596008a0b
Aquib349 <aquib_m@optimusbt.net>
2024-12-30 16:59:02 +0530

    code : new changes

--- src/components/chart/basic-bar-chart.tsx
+++ src/components/chart/basic-bar-chart.tsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import ReactApexChart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
 
 const Basicbarchart = () => {
-  const [chartData, setChartData] = useState({
+  const [chartData, setChartData] = useState<{
+    options: ApexOptions;
+    series: { name: string; data: number[] }[];
+  }>({
     options: {
       chart: {
         id: "basic-bar",
@@ -44,32 +48,30 @@ const Basicbarchart = () => {
           },
         },
       },
-
       plotOptions: {
         bar: {
-          distributed: true, // Allows individual bar colors
+          distributed: true,
           colors: {
-            backgroundBarColors: ["#f3f3f3"], // Background color of bars
-            backgroundBarOpacity: 1, // Opacity of the background
-            backgroundBarRadius: 5, // Rounded background bars
+            backgroundBarColors: ["#f3f3f3"],
+            backgroundBarOpacity: 1,
+            backgroundBarRadius: 5,
           },
         },
       },
       dataLabels: {
         enabled: true,
         formatter: (value, opts) => {
-          // Customize the label text
           const customLabels = ["$90M", "$50M", "$40M", "$20M"];
           return customLabels[opts.dataPointIndex];
         },
         style: {
           fontSize: "12px",
           fontWeight: "bold",
-          colors: ["#000"], // Label color
+          colors: ["#000"],
         },
-        offsetY: -10, // Adjust vertical position of labels
+        offsetY: -10,
       },
-      colors: ["#FFA726", "#FFD54F", "#FFCC80", "#FFEB3B"], // Custom bar colors
+      colors: ["#FFA726", "#FFD54F", "#FFCC80", "#FFEB3B"],
     },
     series: [
       {
